2e976dd5fc01f7d1b1197f9bf8e6c450334d4e03
이진학 <myehdqor@naver.com>
2024-03-25 15:33:12 +0900

    구글로그인api관련 미완성

--- src/main/java/com/green/yogizogi/config/SecurityConfig.java
+++ src/main/java/com/green/yogizogi/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.green.yogizogi.config;
 
+import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,24 +22,27 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin(login->login
+        http.formLogin(login -> login
                 .loginPage("/member/login")
                 .defaultSuccessUrl("/")
                 .loginProcessingUrl("/loginProc")
                 .failureUrl("/member/login/error")
         );
 
-        http.logout(auth-> auth.logoutUrl("/logout")
+        http.logout(auth -> auth.logoutUrl("/logout")
                 .logoutSuccessUrl("/")
         );
 
-        http.authorizeHttpRequests(auth-> auth
+        http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/").permitAll()
                 .anyRequest().permitAll()
         );
-        http.csrf(cs->cs.disable());
+        http.csrf(cs -> cs.disable());
         return http.build();
     }
+
+
 }
