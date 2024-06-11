72d740af2f9f92c995648337b3211d905ec39d03
이진학 <myehdqor@naver.com>
2024-06-11 17:59:53 +0900

    주소보안문제 오류 해결

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,22 +1,18 @@
 /* 다음 주소 연동 */
 function execution_daum_address() {
     new daum.Postcode({
-        oncomplete : function(data) {
-            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-
-            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+        oncomplete: function(data) {
             var addr = ''; // 주소 변수
             var extraAddr = ''; // 참고항목 변수
 
-            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
             if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
                 addr = data.roadAddress;
             } else { // 사용자가 지번 주소를 선택했을 경우(J)
                 addr = data.jibunAddress;
             }
 
-            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+            // 사용자가 선택한 주소가 도로명 타입일 때 참고항목을 조합한다.
             if (data.userSelectedType === 'R') {
                 // 법정동명이 있을 경우 추가한다. (법정리는 제외)
                 // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
@@ -25,48 +21,43 @@ function execution_daum_address() {
                 }
                 // 건물명이 있고, 공동주택일 경우 추가한다.
                 if (data.buildingName !== '' && data.apartment === 'Y') {
-                    extraAddr += (extraAddr !== '' ? ', '
-                        + data.buildingName : data.buildingName);
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                 }
                 // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
                 if (extraAddr !== '') {
                     extraAddr = ' (' + extraAddr + ')';
                 }
-                // 추가해야할 코드
-                // 주소변수 문자열과 참고항목 문자열 합치기
+                // 주소 변수 문자열과 참고항목 문자열 합치기
                 addr += extraAddr;
-
             } else {
                 addr += ' ';
             }
+
+            // 주소를 쿠키에 저장
             setCookie("deliveryAddress1", data.zonecode, 30);
             setCookie("deliveryAddress2", addr, 30);
 
+            // 입력된 주소를 화면에 표시
             $("#deliveryAddress1").val(data.zonecode);
             $("#deliveryAddress2").val(addr);
-
-
         }
     }).open();
-
-
 }
 
-
-//쿠키에 주소 저장 function
+// 쿠키에 주소 저장 function
 var setCookie = function(name, value, exp) {
     var date = new Date();
-    date.setTime(date.getTime() + exp*24*60*60*1000);
-    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
-    location.reload(); // 페이지 새로고침
-
+    date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+    document.cookie = name + '=' + encodeURIComponent(value) + ';expires=' + date.toUTCString() + ';path=/';
 };
 
-//쿠키에서 주소 읽어오기 function
+// 쿠키에서 주소 읽어오기 function
 var getCookie = function(name) {
     var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return value? value[2] : null;
+    return value ? decodeURIComponent(value[2]) : null;
 };
+
+
 
 
 
